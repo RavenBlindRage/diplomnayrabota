5fd8cc22bc55d2cc437757b073216431f90a791e
RavenBlindRage <156050900+RavenBlindRage@users.noreply.github.com>
2024-01-09 12:22:13 +0700

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://5eb384fc-a807-4f6e-8834-f4b16f5c726c.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Naruto",
+    "lastName": "Uzumaki",
+    "address": "Konoha, 1488 apt.",
+    "metroStation": 69,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2022-12-22",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
