e5d837505175c695440d626db758de5e9ca3c7df
RavenBlindRage <156050900+RavenBlindRage@users.noreply.github.com>
2024-01-14 19:01:44 +0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Nikita",
+    "lastName": "Tsvetkov",
+    "address": "Barmaul, 148 apt.",
+    "metroStation": 5,
+    "phone": "+7 800 555 55 55",
+    "rentTime": 5,
+    "deliveryDate": "2022-12-22",
+    "comment": "Sir, luk",
+    "color": [
+        "BLACK"
+    ]
+}
